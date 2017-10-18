d33706f26d1a6af06b25d245b2cd778286424ab2
elicassion <elicassion@sjtu.edu.cn>
2017-10-18 20:49:25 +0800

    init commit. finish 80% basic style.

---
+++ chinese-colors.js
@@ -0,0 +1,20 @@
+data = [
+    {
+        id: 1,
+        name:'玫瑰',
+        cmyk: [47, 91, 70, 10],
+        rgb: [150, 52, 87]
+    },
+    {
+        id: 2,
+        name:'牡丹',
+        cmyk: [35, 100, 82, 2],
+        rgb: [184, 2, 51]
+    }
+]
+
+let colorList = $(".color-list");
+data.forEach((c, i)=>{
+    let document.createElement("label");
+    colorList.appendChild()
+});
